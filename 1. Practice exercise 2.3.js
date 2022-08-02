a9a653a500c805480dba25d76d725e61e8016a89
Marat <80181316+MaratFa@users.noreply.github.com>
2022-08-03 02:02:48 +0300

    Add files via upload

---
+++ 1. Practice exercise 2.3.js
@@ -0,0 +1,4 @@
+// Code to calculate the hypotenuse of a triangle using the Pythagorean theorem when given the values of the other two sides. 
+let a = prompt("Enter the value of the side adjacent to the triangle.");
+let b = prompt("Enter the value of the opposite side to the triangle.");
+console.log((a**2 + b**2) ** 0.5);
