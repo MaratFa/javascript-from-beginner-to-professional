d3ef6f12c04241b2ca16c90fddd3ca13045d7f5d
Marat <80181316+MaratFa@users.noreply.github.com>
2022-08-03 02:29:16 +0300

    My solutuion for Practice exercise 2.4

---
+++ 2. Practice exercise 2.4.js
@@ -0,0 +1,13 @@
+/*
+Create variables for three numbers: a, b, and c. Update these variables with the
+following actions using the assignment operators:
+    - Add b to a;
+    - Divide a by c;
+    - Replace the value of c with the modulus of c and b;
+    - Print all three numbers to the console;
+*/
+let a = 1, b = 2, c = 3;
+a += b;
+a /= c;
+c %= b;
+console.log(a, b, c);
