5fad675221665c50fc6019e4ae64c8b902cd8882
Marat <80181316+MaratFa@users.noreply.github.com>
2022-08-08 05:17:15 +0300

    Solution

---
+++ 5. Exercise 3.1.js
@@ -0,0 +1,12 @@
+/*
+    1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and
+    "Apples."
+    2. Check your list length in the console.
+    3. Update "Bread" to "Bananas."
+    4. Output your entire list to the console.
+*/
+
+const shoppingList = ["Milk", "Bread", "Apples"];
+console.log(shoppingList.length);
+shoppingList[1] = "Bananas";
+console.log(shoppingList);
