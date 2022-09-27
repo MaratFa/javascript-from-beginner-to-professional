d868a71526d05619876343870c672bd94dcc59dc
Marat <80181316+MaratFa@users.noreply.github.com>
2022-09-28 00:34:22 +0300

    Solution

---
+++ 9.4. Exercise 4.3.js
@@ -0,0 +1,10 @@
+/*    
+    1. Create a Boolean value for an ID variable.
+    2. Using a ternary operator, create a message variable that will check whether
+       their ID is valid and either allow a person into a venue or not.
+    3. Output the response to the console.
+*/
+
+let id = true;
+let message = id ? "You are allowed to enter the venue" : "You aren't allowed to enter the venue";
+console.log(message)
