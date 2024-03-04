dce5299da43370f0decdec771341c2ac919d2428
MaratFa <evolution99@rambler.ru>
2024-03-04 18:03:59 +0300

    Solution

---
+++ 9.9.8. Quiz 5.1.js
@@ -0,0 +1,18 @@
+/*
+    -- Answers --
+
+    1. What is expected in the console:
+      0
+      3
+      6
+      9
+    2. What is expected in the console:
+      0
+      5
+      1
+      6
+      2
+      7
+      [1, 5, 7]
+*/
+
