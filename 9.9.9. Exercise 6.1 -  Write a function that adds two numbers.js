489472d78790cc2f8fefa7b0ded0a2757bafc657
MaratFa <evolution99@rambler.ru>
2024-03-07 01:08:13 +0300

    Added

---
+++ 9.9.9. Exercise 6.1 -  Write a function that adds two numbers.js
@@ -0,0 +1,19 @@
+/*
+    -- Description--
+
+    1. Create a function that takes two parameters, adds the parameters together,
+       and returns the result.
+    2. Set up two different variables with two different values.
+    3. Use your function on the two variables, and output the result using
+       console.log.
+    4. Create a second call to the function using two more numbers as arguments
+       sent to the function.
+
+*/
+
+
+function addition(parameter1, parameter2) {
+  return parameter1 + parameter2;
+}
+
+
