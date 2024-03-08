bd6d842f1d8e7036bde2a1c1c47c46f5168d313f
MaratFa <evolution99@rambler.ru>
2024-03-08 23:45:12 +0300

    Added

---
+++ 9.9.9.0. Exercise 6.2 -  Program that will randomly describe an inputted name.js
@@ -0,0 +1,15 @@
+// 1. Create an array of descriptive words.
+
+const descriptiveWordsArray = [];
+
+// 2. Create a function that contains a prompt asking the user for a name.
+
+function userNamePromt() {
+  let userName = prompt("Please, enter your name", "John");
+}
+
+// 3. Select a random value from the array using Math.random.
+
+// 4. Output into the console the prompt value and the randomly selected array value.
+
+// 5. Invoke the function.
