f1e1231f3dc4d0325a95a36684851a7b340f0d33
MaratFa <evolution99@rambler.ru>
2024-03-14 00:10:53 +0300

    Solution

---
+++ 9.9.9.6. Exercise 6.8 - Anonymous function.js
@@ -0,0 +1,14 @@
+/* 1. Set a variable name and assign a function to it. Create a function expression
+with one parameter that outputs a provided argument to the console: */
+
+const variableName = (param) => console.log(param);
+
+// 2. Pass an argument into the function:
+
+variableName("Hello world!");
+
+// 3. Create the same function as a normal function declaration:
+
+function variableName(param) {
+  console.log(param);
+}
