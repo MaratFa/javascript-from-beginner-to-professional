82b33ce8afc7f142f3f08aab1425dd024d9e1d17
MaratFa <evolution99@rambler.ru>
2024-03-14 01:18:31 +0300

    Edited

--- 9.9.9.7. Project 6.1 - Create a recursive function.js
+++ 9.9.9.7. Project 6.1 - Create a recursive function.js
@@ -1,9 +1,9 @@
 /*
-    -- Description--
+  -- Description--
 
-   Create a recursive function that counts up to 10. Invoke the function with different
-   start numbers as the arguments that are passed into the function. The function
-   should run until the value is greater than 10.
+  Create a recursive function that counts up to 10. Invoke the function with different
+  start numbers as the arguments that are passed into the function. The function
+  should run until the value is greater than 10.
 
 */
 
