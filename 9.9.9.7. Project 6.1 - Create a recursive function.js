8881a1d440cca597af4e7a7116e797ccbe4fffb5
MaratFa <evolution99@rambler.ru>
2024-03-14 00:43:59 +0300

    Solution

---
+++ 9.9.9.7. Project 6.1 - Create a recursive function.js
@@ -0,0 +1,20 @@
+/*
+    -- Description--
+
+   Create a recursive function that counts up to 10. Invoke the function with different
+   start numbers as the arguments that are passed into the function. The function
+   should run until the value is greater than 10.
+
+*/
+
+const counter = (val) => {
+  if (val <= 10) {
+    console.log(val);
+    val++;
+    counter(val);
+  }
+};
+
+counter(5);
+
+counter(7);
