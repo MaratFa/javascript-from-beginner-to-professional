7bede8bd110ba2f40c1f014c7c5aac89d624eea8
MaratFa <evolution99@rambler.ru>
2024-03-15 01:04:55 +0300

    Solution

---
+++ 9.9.9.9. Quiz 6.1.js
@@ -0,0 +1,21 @@
+/*
+    -- Answers --
+
+    1. What value is output into the console:
+      10
+
+    2. What will be output into the console:
+      Hello
+
+    3. What will be output into the console:
+      Welcome
+      Laurence
+      My Name is Laurence
+    
+    4. What will be output to the console:
+      19
+
+    5. What will be output to the console:
+      16
+
+*/
