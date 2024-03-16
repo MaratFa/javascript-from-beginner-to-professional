277fa1a4fbdb49c7630eb86eb0d0f71c8725a24a
MaratFa <evolution99@rambler.ru>
2024-03-16 23:03:10 +0300

    Solution

---
+++ 9.9.9.9.0. Exercise 7.1 - Create a person class, and print instances of friends' names.js
@@ -0,0 +1,21 @@
+// 1. Create a class for Person including the constructor for firstname and lastname:
+
+class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
+
+// 2. Create a variable and assign a value of the new Person object using your friend's first and last names:
+
+let person1 = new Person("John", "Johnson");
+
+// 3. Now add a second variable with another friend's name using their first name and last name:
+
+let person2 = new Person("Jackie", "Chan");
+
+// 4. Output both friends into the console with a greeting of hello:
+
+console.log("Hello", person1.firstname, person1.lastname);
+console.log("Hello", person2.firstname, person2.lastname);
