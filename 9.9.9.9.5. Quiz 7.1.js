08094d647e84f7bf1e51ec911853e2bf26f1aa88
MaratFa <evolution99@rambler.ru>
2024-03-18 10:20:14 +0300

    Solution

---
+++ 9.9.9.9.5. Quiz 7.1.js
@@ -0,0 +1,33 @@
+/*
+    -- Answers --
+
+    1. What is the keyword used to create a class:
+
+      class
+
+    2. How would you set up a class for a person's first and last names that could include first and last as initial properties:
+
+      class Person {
+        constructor (firstName, lastName) {
+          this.firstName = firstName;
+          this.lastName = lastName;
+        }
+      }
+    
+    3. What is the concept of one thing gaining the properties and behaviors of another thing called:
+
+      Inheritance
+
+    4. Which of the following are true about the constructor method:
+
+      True
+      False
+      True
+      True
+      False
+
+    5. Troubleshoot the following code so that the prototype outputs the first and last name of the Person into the console. Which is the correct syntax for the Person prototype:
+    
+      B)
+
+*/
