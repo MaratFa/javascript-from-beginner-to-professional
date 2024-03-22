446947b4f6553d3837720c0636fb2cbeafe1f9cd
MaratFa <evolution99@rambler.ru>
2024-03-22 22:57:51 +0300

    Edited

---
+++ 9.9.9.9.9. Exercise 8.4 - Using string manipulation, create a function ... .js
@@ -0,0 +1,37 @@
+/*  
+Using string manipulation, create a function that will return a string with the first
+letter of all the words capitalized and the rest of the letters in lowercase. You should
+transform the sentence thIs will be capiTalized for each word into This Will Be
+Capitalized For Each Word:
+*/
+
+/* 1. Create a string with several words that have letters with different cases, a
+mix of upper and lowercase words: */
+
+/* 2. Create a function that gets a string as an argument, which will be the value
+that we will manipulate: */
+
+// 3. In the function first transform everything to lowercase letters:
+
+/* 4. Create an empty array that can hold the values of the words when we
+capitalize them: */
+
+// 5. Convert the phrase into words in an array using the split() method:
+
+/* 6. Loop through each one of the words that are now in the new array, so you
+can select each one independently. You can use forEach() for this: */
+
+/* 7. Using slice() isolate the first letter in each word, then transform it to
+uppercase. Again using slice(), get the remaining value of the word
+without including the first letter. Then concatenate the two together to form
+the word that is now capitalized: */
+
+/* 8. Add the new capitalized word into the blank array that you created. By the
+end of the loop you should have an array with all the words as separate
+items in the new array: */
+
+/* 9. Take the array of updated words and using the join() method, transform
+them back into a string with spaces between each word: */
+
+/* 10. Return the value of the newly updated string with capitalized words that can
+then be output into the console: */
