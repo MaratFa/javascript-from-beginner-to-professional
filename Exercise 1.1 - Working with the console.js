fc025641963b733dbb4f48b305f06d50eac581d5
MaratFa <evolution99@rambler.ru>
2024-05-18 04:03:44 +0300

    Added

---
+++ Exercise 1.1 - Working with the console.js
@@ -0,0 +1,7 @@
+/*  
+    1. Open the browser console, type 4 + 10, and press Enter. What do you see as
+       the response?
+    2. Use the console.log() syntax, placing a value within the rounded brackets.
+       Try entering your name with quotes around it (this is to indicate the fact that
+       it's a text string—we'll get to this in the next chapter).
+*/
