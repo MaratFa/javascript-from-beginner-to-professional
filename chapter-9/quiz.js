c54cb2c2a9c84560e533a452a623133e55aa78d5
MaratFa <evolution99@rambler.ru>
2024-05-20 00:12:29 +0300

    Added

---
+++ chapter-9/quiz.js
@@ -0,0 +1,14 @@
+/*
+1. Go to your favorite website and open the browser console. Type document.
+   body. What do you see in the console?
+2. As we know, with objects, we can write to the property value and assign a
+   new value with the assignment operator. Update the textContent property
+   of the document.body object on a web page of your choosing to contain the
+   string Hello World.
+3. Use what we learned about objects to list out BOM object properties and
+   values. Try it on the document object.
+4. Now do the same for the window object.
+5. Create an HTML file with an h1 tag. Use JavaScript and select the page
+   element with the h1 tag and assign the element into a variable. Update the
+   textContent property of the variable to Hello World.      
+*/
