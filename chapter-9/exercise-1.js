7f07d0dbe2d49f8c0cee8fb7ceb13ea6bc19d13e
MaratFa <evolution99@rambler.ru>
2024-05-20 00:12:21 +0300

    Added

---
+++ chapter-9/exercise-1.js
@@ -0,0 +1,9 @@
+/*  
+1. Go back to the website you were just viewing and execute the command
+console.dir(window).
+2. Can you find the document object that is nested within the window object?
+Under the root of the window object in the console, you can navigate down to
+the object that is named document.
+3. Can you find the height and width (in pixels) of your window? You can
+return the inner and outer window.
+*/
