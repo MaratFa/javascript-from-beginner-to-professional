ff92f04a5f39f0ef1a26b75ea15c60ceb6927b19
MaratFa <evolution99@rambler.ru>
2024-05-20 00:11:10 +0300

    Renamed

--- chapter-8/Quiz 8.1.js
+++
@@ -1,9 +0,0 @@
-/*
-    -- Answers --
-
-    1. decodeURIComponent(e)
-    2. 4
-    3. ["Hii","hi","hello","Hii","hi","hi World","Hi"]
-    4. ["hi","hi World"]
-      
-*/
--- chapter-9/Exercise 9.2.js
+++
@@ -1,4 +0,0 @@
-/*  
-Travel through the window object to get to the location object, then output the values of the protocol and href properties of the current file, into the console.
-*/
-
