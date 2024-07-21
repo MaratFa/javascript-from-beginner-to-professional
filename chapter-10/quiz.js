9c91a311bb98045e0c0d1ab3c2ce5f1647faac67
MaratFa <evolution99@rambler.ru>
2024-07-21 20:49:10 +0300

    solutions for quiz

---
+++ chapter-10/quiz.js
@@ -0,0 +1,16 @@
+/*  
+    1. Hello <br> World
+
+    2. Hello
+       World
+
+    3. Hello World
+
+    4. three
+    
+       one
+       two
+       three
+
+    5. btn.removeEventListener("click", myFun);
+*/
